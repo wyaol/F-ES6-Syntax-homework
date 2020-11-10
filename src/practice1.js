95303320eedc2e83a5761c79204681c8c5fdccfb
wyao <289672494@qq.com>
2020-11-10 20:28:12 +0800

    [wang yao] feature: practice2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,9 @@
-const parseData
+const parseData = (input) => {
+  const {data, column} = input;
+  return data.map(item => {
+    const res = {};
+    column.forEach((property, index) => Object.assign(res, {[property.name]: item[index]}));
+    return res;
+  })
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,10 @@
-const inject
+const inject = (items, sections) => {
+  let i = 0;
+  sections.forEach(section =>{
+    const {content, index} = section;
+    items.splice(index + i, 0, content);
+    i++;
+  })
+  return items;
+}
 export { inject };
